75b2627868392d21ea6b38a9c9324e46d3f21bde
Fred Israel <fredpublico@gmail.com>
2018-09-30 22:14:13 -0300

    supports json now

--- accumulator.go
+++ accumulator.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "time"
-    //"encoding/json"
+    "encoding/json"
     "io"
     "os"
     "strconv"
@@ -37,61 +37,124 @@ type request_w_handle struct {r *http.Request; handle chan []byte}
 
 func enable_accumulation(next http.HandlerFunc) http.HandlerFunc {
     var main_chan = make(chan request_w_handle)
-    go accumulator(main_chan, next)
+    go accumulator(main_chan, next, get_writer_type())
     return func(w http.ResponseWriter, r *http.Request) {
         var handle = make(chan []byte)
         main_chan <- request_w_handle{r, handle}
         log.Printf("Tracing request for %s", r.RequestURI)
         var result = <- handle
+        log.Printf("Returning request for %s", result)
         w.Write(result)
     }
 }
 
+func get_writer_type() func(io.Writer) batch_writer{
+    content_type, defined := os.LookupEnv("CONTENT_TYPE")
+    if(defined == false){
+        content_type = "application/json"
+    }
+    switch content_type {
+        case "application/json":
+            return NewJsonWriter
+        case "multipart/form-data":
+            return NewMultipartWriter
+        default:
+            log.Panic("CANNOT HANDLE CONTENT_TYPE: " + content_type)
+    }
+    return nil
+}
+
+
+type batch_reader interface {
+    Next() ([]byte, error)
+}
+
+type batch_json_reader struct {
+    jsons []json.RawMessage
+    counter int
+}
+type batch_multipart_reader struct {
+    *multipart.Reader
+}
+func NewBatchReader(body io.Reader, header http.Header) batch_reader {
+    content_type := header.Get("Content-Type")
+    switch content_type {
+        case "application/json":
+            decoder := json.NewDecoder(body)
+            var out []json.RawMessage
+            decoder.Decode(&out)
+            return &batch_json_reader{out, 0}
+        case "multipart/form-data":
+            _, content_type_map, _ := mime.ParseMediaType(content_type)
+            return batch_multipart_reader{multipart.NewReader(body, content_type_map["boundary"])}
+        default:
+            b, _ := ioutil.ReadAll(body)
+            log.Println("BAD RESPONSE: " + string(b))
+            log.Panic("CANNOT HANDLE CONTENT_TYPE IN RESPONSE: " + content_type)
+            return nil
+    }
+}
+func (self batch_multipart_reader) Next() ([]byte, error){
+    part, err := self.NextPart()
+    if(err!=nil) {return nil, err}
+    part_content, err2 := ioutil.ReadAll(part)
+    return part_content, err2
+}
+func (self *batch_json_reader) Next() ([]byte, error){
+    ret := []byte(self.jsons[self.counter])
+    self.counter++
+    return ret, nil
+}
+
+
 type batch_writer interface {
     WriteField(string, string) error
     Close() error
-    Boundary() string
+    ContentType() string
 }
 
-type multipart_writer batch_writer
-
-func NewMultipartWriter(buffer io.Writer) multipart_writer {
-    return multipart.NewWriter(buffer)
+type multipart_writer struct {
+    *multipart.Writer
 }
+
+func NewMultipartWriter(buffer io.Writer) batch_writer {
+    return multipart_writer{multipart.NewWriter(buffer)}
+}
+func (self multipart_writer) ContentType() string { return "multipart/form-data; boundary=" + self.Boundary()}
 
 type json_writer struct {
     buffer io.Writer
     first bool;
 }
 
-func NewJsonWriter(buffer io.Writer) *json_writer {
-    r := new(json_writer)
-    r.first = true
-    r.buffer = buffer
-    return r
+func NewJsonWriter(buffer io.Writer) batch_writer {
+    r := json_writer{first: true, buffer: buffer}
+    return &r
 }
 
-func (json json_writer) Boundary() string { return "" }
-func (json json_writer) Close() error { return nil}
-func (json json_writer) WriteField(request string, boundary string) error {
-    if(!json.first) {
-        json.buffer.Write([]byte(","))
+func (self json_writer) ContentType() string {return "application/json"}
+func (self json_writer) Close() error {
+    _, err := self.buffer.Write([]byte("]"))
+    return err
+}
+func (self *json_writer) WriteField(index string, request string) error {
+    if(self.first == true) {
+        self.buffer.Write([]byte("[\n"))
+        self.first = false
+    } else {
+        self.buffer.Write([]byte(","))
     }
-    json.first = false
-    json.buffer.Write([]byte(request))
+    log.Println(request)
+    self.buffer.Write([]byte(request))
     return nil
 }
 
-
-
-
 const BATCH int = 3
 const TIMEOUT = 3
-func accumulator(main_chan chan request_w_handle, next http.HandlerFunc){
+func accumulator(main_chan chan request_w_handle, next http.HandlerFunc, batch_writer func(io.Writer) batch_writer){
     for {
         request_body := new(bytes.Buffer)
-        //var writer batch_writer = multipart.NewWriter(request_body)
-        var writer batch_writer = NewJsonWriter(request_body)
+        writer := batch_writer(request_body)
         var handles [BATCH]chan []byte
         timeout := time.After(TIMEOUT * time.Second)
         var i int
@@ -109,15 +172,12 @@ func accumulator(main_chan chan request_w_handle, next http.HandlerFunc){
         batch_size := i
         if (batch_size == 0) {continue}
         writer.Close()
-        response := make_request(request_body, writer.Boundary(), next)
-        _, params, err := mime.ParseMediaType(response.Header().Get("Content-Type"))
-        if (err != nil) {panic("DAMN!")};
-        var reader = multipart.NewReader(response.Body, params["boundary"])
+        response := make_request(request_body, writer.ContentType(), next)
+        var reader = NewBatchReader(response.Body, response.Header())
         for i:= 0; i< batch_size; i++ {
-            var part, err = reader.NextPart()
+            var part, err = reader.Next()
             if(err == nil) {
-                var part_content, _ = ioutil.ReadAll(part)
-                handles[i] <- part_content
+                handles[i] <- []byte(part)
             } else {
                 handles[i] <- []byte(err.Error())
             }
@@ -125,10 +185,10 @@ func accumulator(main_chan chan request_w_handle, next http.HandlerFunc){
     }
 }
 
-func make_request(request_body io.Reader, boundary string, next http.HandlerFunc) *httptest.ResponseRecorder {
+func make_request(request_body io.Reader, content_type string, next http.HandlerFunc) *httptest.ResponseRecorder {
     var response = httptest.NewRecorder()
     var request = httptest.NewRequest("POST", "/", request_body)
-    request.Header.Set("Content-Type", "multipart/form-data; boundary=" + boundary)
+    request.Header.Set("Content-Type", content_type)
     log.Println("Sending request")
     next.ServeHTTP(response, request)
     return response
--- doubler.py
+++ doubler.py
@@ -1,12 +1,25 @@
 import flask
-import time
+import time, json
 from requests_toolbelt import MultipartEncoder
 
 app = flask.Flask('')
 
 @app.route('/', methods=['POST'])
-def echo():
+def doubler():
+    print("%%%REQUEST: " +str(flask.request.get_data()) + str(flask.request.__dict__))
     time.sleep(1) # this is a very hard computation
+    if flask.request.content_type == 'application/json':
+        ret = json_doubler()
+    elif 'multipart/form-data' in flask.request.content_type:
+        ret = multipart_doubler()
+    print("%%%RETURNING: " + str(ret.__dict__))
+    return ret
+
+def json_doubler():
+    doubled = [int(v) * 2 for v in flask.request.json]
+    return flask.Response(content_type='application/json', response=json.dumps(doubled))
+
+def multipart_doubler():
     doubled = {k: str(int(v) * 2) if v else str(int(k) * 2) for k, v in flask.request.form.items()}
     m = MultipartEncoder(fields=doubled)
     return flask.Response(content_type=m.content_type, response=m.to_string())
